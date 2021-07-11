e7a34b048b7d24eeec2a563847015d16152f7cac
bishalkharel <bishalkharel0000@gmail.com>
2021-07-11 16:22:21 +0000

    Initial commit

---
+++ nepse.py
@@ -0,0 +1,102 @@
+import requests as rq
+import os
+from bs4 import BeautifulSoup
+import pandas as pd
+from colorama import Fore,Style,Back,init
+
+
+to_nepse=f"http://www.nepalstock.com/main/todays_price/index/"
+
+init(autoreset=True)
+
+# variable declaration
+
+titles=[]
+SN=[]
+traded_comp=[]
+no_of_trans=[]
+max_price=[]
+min_price=[]
+closing_price=[]
+traded_shares=[]
+amount=[]
+prev_closing=[]
+difference=[]  
+
+
+
+def pd_columns():
+    global titles
+    for firstpage in range(1):
+        res=rq.get(f"{to_nepse}{firstpage}").text
+        souped_data=BeautifulSoup(res,'html5lib')
+        main_table=souped_data.findAll('table',attrs={'class':'table table-condensed table-hover'})[0]
+        #print(main_table)
+        for tds in main_table.find_all('tr',{'class':'unique'}):
+            for td in (tds.find_all("td")):
+                titles.append(td.getText())
+                #print(td.getText())
+    titles.remove('S.N.')
+
+
+def scrap():
+    
+    for page_indexing in range(12):
+        to_skip=0
+        vals=[]
+        res=rq.get(f'{to_nepse}{page_indexing+1}').content.decode('utf-8')
+        souped_data=BeautifulSoup(res,'html.parser')
+        main_table=souped_data.find_all('table',{'class':'table table-condensed table-hover'})[0]
+        for trs in main_table.find_all('tr'):
+            if to_skip<2:
+                to_skip+=1
+                continue
+            else:
+                try:
+                    vals=trs.find_all('td')
+                    #SN.append(vals[0].getText())
+                    traded_comp.append(vals[1].getText())
+                    print(f'{Fore.GREEN}{Style.BRIGHT}[-]  {Fore.WHITE}{Style.BRIGHT}{vals[1].getText()}')
+                    no_of_trans.append(vals[2].getText())
+                    max_price.append(vals[3].getText())
+                    min_price.append(vals[4].getText())
+                    closing_price.append(vals[5].getText())
+                    traded_shares.append(vals[6].getText())
+                    amount.append(vals[7].getText())
+                    prev_closing.append(vals[8].getText())
+                    difference.append(vals[9].getText())
+                except IndexError:
+                    break
+
+def createCSV(file_name):
+    df=pd.DataFrame(columns=titles)
+    df["Traded Companies"]=traded_comp
+    df["No. Of Transaction"]=no_of_trans
+    df["Max Price"]=max_price
+    df["Min Price"]=min_price
+    df["Closing Price"]=closing_price
+    df["Traded Shares"]=traded_shares
+    df["Amount"]=amount
+    df["Previous Closing"]=prev_closing
+    df["Difference Rs."]=difference
+    decode_unicode=df['Difference Rs.'].str.split().str.join(' ')
+    df["Difference Rs."]=decode_unicode
+    df.drop(df.filter(regex="Unname"),axis=1, inplace=True)
+    df.to_csv(f"{file_name}.csv",index=False)
+    print(f" saved your file under {Fore.RED}{Style.BRIGHT}{file_name}.csv ")
+
+
+if  __name__=="__main__":
+    
+    pd_columns()
+    scrap()
+    yes_or_no=input("Do you want to save your file (y/n):  ")
+    
+    if yes_or_no.lower()=="y":
+        file_name=input("Enter your file name: ")
+        createCSV(file_name)
+           
+    else:
+        print("Okay exiting...")
+        exit()
+
